f34f03605357107b5c125f8813f7dc83a31dc085
AndreaDex <desiderio.andrea@outlook.it>
2021-06-25 16:47:20 +0200

    create an obj, array of objs

--- index.php
+++ index.php
@@ -12,22 +12,41 @@ class Movie
     }
 
     public function getRatings(){
-        echo $this->ratings;
+        return $this->ratings;
     }
+
     /**
      * Crea un oggetto film
-     * @param string $titile Titolo del film
+     * @param string $title Titolo del film
      * @param string $genre Genere del film  
      * @param int $year Anno di uscita
      * @param int $time Durata
      */
-    public function __constructor(string $titile, string $genre, int $year, int $time){
-        $this->$titile = $titile;
-        $this->$genre = $genre;
-        $this->$year = $year;
-        $this->$time = $time;
+    public function __construct(string $title, string $genre, int $year, int $time){
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->year = $year;
+        $this->time = $time;
     }
 
 
-}
+};
+
+
+$aQuiet = new Movie('A Quiet Place ','Horror', 2018, 91);
+$aQuiet->setRatings(6);
+var_dump($aQuiet);
+var_dump($aQuiet->getRatings());
+
+
+$latest = [
+    new Movie('A Quiet Place 2','Horror', 2021, 91),
+    new Movie('SCHOOL OF MAFIA','Commedia', 2021, 90),
+    new Movie('World War Z 2','Azione', 2021, 101),
+    new Movie('Fast & Furious 9','Azione', 2021, 145),
+    new Movie('Il Cattivo Poeta','Biografico', 2021, 106)
+];
+
+var_dump($latest);
+
 
